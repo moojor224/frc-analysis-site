011c6e8613541a7132f18748cdbdc59fc4af7662
moojor224 <moojor224@gmail.com>
2026-02-07 00:44:14 -0600

    Potential fix for PWA detection

--- src/components/PWA.tsx
+++ src/components/PWA.tsx
@@ -5,13 +5,14 @@ export default function PWA() {
     const [canPWA, setCanPWA] = useState(false);
     const [event, setEvent] = useState<(Event & { prompt(): void }) | null>(null);
     useEffect(() => {
-        window.addEventListener("beforeinstallprompt", function (e) {
+        // @ts-ignore
+        window.onbeforeinstallprompt = function (e: any) {
             console.debug("app is valid pwa");
             if (location.protocol === "https:" || location.hostname === "localhost") {
                 setCanPWA(true);
-                setEvent(e as any);
+                setEvent(e);
             }
-        });
+        };
     }, []);
     return (
         <Button sx={{ display: canPWA ? "block" : "none" }} onClick={() => event?.prompt?.()} variant="outlined">
