823a30a60b9c0682b1385380eb346d7f5d37d52b
moojor224 <moojor224@gmail.com>
2026-02-16 14:43:50 -0600

    Remove unused imports

--- src/app/app_structure/createAnalyticsPagePipeline.tsx
+++ src/app/app_structure/createAnalyticsPagePipeline.tsx
@@ -1,21 +1,9 @@
-import { Container, Paper, Stack } from "@mui/material";
-import { useContext, useState } from "react";
-import { Tabs } from "../page.js";
-import { ApiContext, TBAAPI } from "../../lib/tba_api/index.js";
-import { useInstanceManager } from "./analytics_page/useInstanceManager.js";
-import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-import KeyboardArrowUp from "@mui/icons-material/KeyboardArrowUp";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Tab from "@mui/material/Tab";
-import type { TabScrollButtonProps } from "@mui/material/TabScrollButton";
-import MaterialTabs from "@mui/material/Tabs";
 import React from "react";
-import { Pipeline, Step } from "./pipeline/index.js";
-import { PipelineRenderer } from "./pipeline/PipelineRenderer.js";
+import { TBAAPI } from "../../lib/tba_api/index.js";
+import { Tabs } from "../page.js";
 import { createAnalyticsPage } from "./createAnalyticsPage.js";
+import { Pipeline } from "./pipeline/index.js";
+import { PipelineRenderer } from "./pipeline/PipelineRenderer.js";
 
 /**
  * simple createAnalyticsPage wrapper to make the inputs section pipeline-generated
